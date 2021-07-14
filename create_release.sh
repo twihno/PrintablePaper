4233dee54df1b2ba37aea9cc96653d449b61f9ff
Thomas Schuster <twihno@gmail.com>
2021-07-14 17:14:52 +0200

    updated create_release script

--- create_release.sh
+++ create_release.sh
@@ -9,6 +9,21 @@ print_return_value () {
     fi
 }
 
+# Checking/generating build name
+echo -e "\n\e[32m\e[1m*** Checking/generating build name ***\e[0m"
+
+# Get build_name
+build_name=$1
+
+if [ -z ${build_name} ]; then
+    build_name="dev$(date +'%Y-%m-%d-%H:%M')"
+    echo -e "\nGenerated build_name: $build_name"
+    echo -e "\n  ✅ \e[32m\e[1mGENERATED BUILD NAME\e[0m"
+else
+    echo -e "\nFound build_name: $build_name"
+    echo -e "\n  ✅ \e[32m\e[1mBUILD NAME FOUND\e[0m"
+fi
+
 # Create pdf files
 echo -e "\n\e[32m\e[1m*** Run build.sh ***\e[0m"
 ./build.sh
@@ -25,7 +40,7 @@ print_return_value $?
 # Create zip file
 echo -e "\n\e[32m\e[1m*** Create zip file ***\e[0m"
 cd output
-zipname="PrintablePaper_$1.zip"
+zipname="PrintablePaper_$build_name.zip"
 zipname="${zipname//$'/'/'_'}" # Remove / from zipname
 zip -r $zipname *
 print_return_value $?
